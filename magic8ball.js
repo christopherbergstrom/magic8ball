e2b8e34e0494b4087b59a5a4e9557a332245b313
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-06-05 12:43:24 -0600

    added funny answers

--- magic8ball.js
+++ magic8ball.js
@@ -24,8 +24,7 @@ function animate()
   eightBall.style.backgroundColor = "white";
   var wait4 = setTimeout(function()
   {
-    response("Classic");
-    // response(type.value);
+    response(type.value);
   }, 500);
   var wait5 = setTimeout(function()
   {
@@ -41,18 +40,12 @@ function response(value)
   }
   else if (value === "Funny")
   {
-    var num = Math.floor(Math.random() * 20);
+    var num = Math.floor(Math.random() * 22);
     answer.innerHTML=funny[num];
-  }
-  else if (value === "Crude")
-  {
-    var num = Math.floor(Math.random() * 20);
-    answer.innerHTML=crude[num];
   }
 }
 function makeArrays()
 {
   classic = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
-  funny = [];
-  crude = [];
+  funny = ["I guess so...", "If you try hard enough", "Sure", "Anything is possible!", "It might happen", "Fingers crossed", "I'll say yes if you hand me a Benjamin Franklin", "Did you really just ask that?", "Of course not", "I don't think so", "Please tell me you're joking", "Probably not...", "Yeah right...", "In your dreams!", "I'm not sure", "I'm not here right now, please try again later", "I can't see that far in the future", "Check your Wi-Fi connection", "System error", "Null pointer exception", "StackOverFlowError", "Sorry, I'm having trouble loading"];
 }
