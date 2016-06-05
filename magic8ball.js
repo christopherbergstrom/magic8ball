44cc060b869654b844ae6d95e4a8b0a1a71ed609
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-06-04 23:36:20 -0600

    everything done

--- magic8ball.js
+++ magic8ball.js
@@ -14,11 +14,23 @@ window.onload = function()
   eightBall = document.getElementById("eightBall");
   eightBall.addEventListener("click", function()
   {
-    // console.log(type.value);
-    response(type.value);
+    animate();
   });
   makeArrays();
-  // console.log("loaded");
+}
+function animate()
+{
+  eightBall.style.transition = ".5s linear";
+  eightBall.style.backgroundColor = "white";
+  var wait4 = setTimeout(function()
+  {
+    response("Classic");
+    // response(type.value);
+  }, 500);
+  var wait5 = setTimeout(function()
+  {
+    eightBall.style.backgroundColor = "black";
+  }, 1000);
 }
 function response(value)
 {
@@ -27,38 +39,20 @@ function response(value)
     var num = Math.floor(Math.random() * 20);
     answer.innerHTML=classic[num];
   }
-  else if (value === "Classic")
+  else if (value === "Funny")
   {
-
+    var num = Math.floor(Math.random() * 20);
+    answer.innerHTML=funny[num];
   }
-  else if (value === "Classic")
+  else if (value === "Crude")
   {
-
+    var num = Math.floor(Math.random() * 20);
+    answer.innerHTML=crude[num];
   }
 }
 function makeArrays()
 {
   classic = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+  funny = [];
+  crude = [];
 }
-// Classic
-
-// It is certain
-// It is decidedly so
-// Without a doubt
-// Yes, definitely
-// You may rely on it
-// As I see it, yes
-// Most likely
-// Outlook good
-// Yes
-// Signs point to yes
-// Reply hazy try again
-// Ask again later
-// Better not tell you now
-// Cannot predict now
-// Concentrate and ask again
-// Don't count on it
-// My reply is no
-// My sources say no
-// Outlook not so good
-// Very doubtful
