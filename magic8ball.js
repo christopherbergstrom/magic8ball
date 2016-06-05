c4cfce04a97ed1f3633bba1f0dfd36459d44032c
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-06-04 18:43:32 -0600

    getting basic logic done

--- magic8ball.js
+++ magic8ball.js
@@ -1,4 +1,64 @@
+var type;
+var body;
+var eightBall;
+var answer;
+var classic = [];
+var funny = [];
+var crude = [];
+
 window.onload = function()
 {
-  console.log("loaded");
+  body = document.querySelector("body");
+  type = document.getElementById("type");
+  answer = document.getElementById("answer")
+  eightBall = document.getElementById("eightBall");
+  eightBall.addEventListener("click", function()
+  {
+    // console.log(type.value);
+    response(type.value);
+  });
+  makeArrays();
+  // console.log("loaded");
 }
+function response(value)
+{
+  if (value === "Classic")
+  {
+    var num = Math.floor(Math.random() * 20);
+    answer.innerHTML=classic[num];
+  }
+  else if (value === "Classic")
+  {
+
+  }
+  else if (value === "Classic")
+  {
+
+  }
+}
+function makeArrays()
+{
+  classic = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+}
+// Classic
+
+// It is certain
+// It is decidedly so
+// Without a doubt
+// Yes, definitely
+// You may rely on it
+// As I see it, yes
+// Most likely
+// Outlook good
+// Yes
+// Signs point to yes
+// Reply hazy try again
+// Ask again later
+// Better not tell you now
+// Cannot predict now
+// Concentrate and ask again
+// Don't count on it
+// My reply is no
+// My sources say no
+// Outlook not so good
+// Very doubtful
